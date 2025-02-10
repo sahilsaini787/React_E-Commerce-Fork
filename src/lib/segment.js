562c484df195da008f1d2df18608d03a2425451a
Saksham9944 <sakshamj9944@gmail.com>
2025-02-10 10:30:58 +0000

    added script in the html page

--- src/lib/segment.js
+++ src/lib/segment.js
@@ -2,6 +2,6 @@ import { AnalyticsBrowser } from '@segment/analytics-next'
 
 
 const analytics = new AnalyticsBrowser()
-analytics.load({ writeKey: 'yEVKwWFjOEqjTWJbq7iN8IxkJ5dghbB1' })
+analytics.load({ writeKey: process.env.REACT_APP_SEGMENT_SOURCE_KEY})
 
 export default analytics
